e68fd730b26fd5c094c12c88b29f283089ad9751
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2024-03-02 22:09:47 +0200

    ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  // типізація стейту
+  // якщо стейт не типізувати, він автоматично запам'ятає тип при ініціалізації, тобто мжна не вказувати тип
+  // або ж вказати, тим паче якщо треба типізувати декілька значень
+  const [input, setInput] = useState<string | number>()
   // типізацію івенту(події)
   // пожний івент треба типізувати - e:React.FormEvent<HTMLFormElement> (кожен по своєму)
   const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
+
+    
+    setInput(e.currentTarget[0].value);
+    
     e.currentTarget.reset()
   }
   const handleChangeInput = (e:React.ChangeEvent<HTMLInputElement>) => {
@@ -13,7 +23,6 @@ function App() {
   }
   const handleButtonClick = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     console.log(e.target);
-    
   }
 
   return (
